2b5cd6aec2d8039ba692eb3f637c54398ac38ca7
Isac Vitor <isac24vitor@gmail.com>
2025-02-21 16:36:43 -0300

    resolvendo problema de cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const port =5000
 const app = express()
 
 // app.use(cors())
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(cors({ credentials: true, origin: 'https://teodoteca.onrender.com' }))
 app.use(express.json())
 app.use(express.static('public'))
 
