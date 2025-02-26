42b2d6e4e5c1543d61f2054602f8ebdc72d4ad9d
Isac Vitor <isac24vitor@gmail.com>
2025-02-25 21:25:57 -0300

    corrigindo bug de rota

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export default axios.create({
-    baseURL:'https://teodoteca.vercel.app',
+    baseURL:'https://teodoteca.onrender.com',
     // baseURL:'http://localhost:5000',
 })
